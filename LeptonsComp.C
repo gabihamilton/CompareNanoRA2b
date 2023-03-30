d172fd2b0b5636e0484ce9a87b7cccbbd7759f45
gabihamilton <gabihamilton@gmail.com>
2023-03-30 16:27:44 -0500

    put some legend

--- LeptonsComp.C
+++ LeptonsComp.C
@@ -9,12 +9,15 @@ vector<int> filterLeptons(Float_t* pt, double pt_threshold){
   vector<int> indices;
   for( int i = 0 ; i < n ; i++ ){
     if( pt[i] > pt_threshold){
-      cout << "i: " << i << " pt: " << pt[i] << endl;
+      //cout << "i: " << i << " pt: " << pt[i] << endl;
       indices.push_back(i);
     }
   }
   return indices;
 }
+
+
+
 
 void LeptonsComp(){
 
@@ -32,12 +35,12 @@ void LeptonsComp(){
   vector<TH1F*> e_pt_nano;
   for( int i = 0 ; i < 4 ; i++ ){
     sprintf(temp,"Electronpt_ra2b%i",i);
-    e_pt_ra2b.push_back(new TH1F(temp,";Electron p_T [GeV];Events",50,0,800));
+    e_pt_ra2b.push_back(new TH1F(temp,";Electron p_T [GeV];Events",100,0,800));
     e_pt_ra2b.back()->SetLineWidth(2);
     e_pt_ra2b.back()->SetLineStyle(1);
     e_pt_ra2b.back()->SetLineColor(2);
     sprintf(temp,"Electronpt_nano%i",i);
-    e_pt_nano.push_back(new TH1F(temp,";Electron p_T [GeV];Events",50,0,800));
+    e_pt_nano.push_back(new TH1F(temp,";Electron p_T [GeV];Events",100,0,800));
     e_pt_nano.back()->SetLineWidth(2);
     e_pt_nano.back()->SetLineStyle(2);
     e_pt_nano.back()->SetLineColor(4);
@@ -47,12 +50,12 @@ void LeptonsComp(){
   vector<TH1F*> e_eta_nano;
   for( int i = 0 ; i < 4 ; i++ ){
     sprintf(temp,"Electroneta_ra2b%i",i);
-      e_eta_ra2b.push_back(new TH1F(temp,";Electron #eta [GeV];Events",20,-5,5));
+      e_eta_ra2b.push_back(new TH1F(temp,";Electron #eta [GeV];Events",100,-5,5));
       e_eta_ra2b.back()->SetLineWidth(2);
       e_eta_ra2b.back()->SetLineStyle(1);
       e_eta_ra2b.back()->SetLineColor(2);
     sprintf(temp,"Electroneta_nano%i",i);
-      e_eta_nano.push_back(new TH1F(temp,";Electron #eta [GeV];Events",20,-5,5));
+      e_eta_nano.push_back(new TH1F(temp,";Electron #eta [GeV];Events",100,-5,5));
       e_eta_nano.back()->SetLineWidth(2);
       e_eta_nano.back()->SetLineStyle(2);
       e_eta_nano.back()->SetLineColor(4);
@@ -167,7 +170,8 @@ void LeptonsComp(){
         pho_phi_nano.back()->SetLineColor(4);
     }
 
-    
+    int num_electron_nano=0;
+    int num_electron_ra=0;
 
   int numEvents = ra2b_t->fChain->GetEntriesFast();
   for( int evt = 0 ; evt < numEvents ; evt++ ){
@@ -185,13 +189,16 @@ void LeptonsComp(){
       vector<int> nano_indices_p = filterLeptons(nano_t->Photon_pt, 30.0);
 
     //cout << "num good jets :: ra2b " << ra2b_indices.size() << " nano " << nano_indices.size() << endl;
-    
+
     for( int j= 0 ; j < 4 ; j++ ){
       if( j < ra2b_indices_e.size() ){
         e_pt_ra2b[j]->Fill(ra2b_t->Electrons_fCoordinates_fPt[ra2b_indices_e[j]]);
         e_eta_ra2b[j]->Fill(ra2b_t->Electrons_fCoordinates_fEta[ra2b_indices_e[j]]);
         e_phi_ra2b[j]->Fill(ra2b_t->Electrons_fCoordinates_fPhi[ra2b_indices_e[j]]);
+          num_electron_ra = num_electron_ra + 1;
       }if( j < nano_indices_e.size() ){
+          //if (nano_t->Electron_cutBased[j]>0) e_pt_nano[j]->Fill(nano_t->Electron_pt[nano_indices_e[j]]);
+          num_electron_nano = num_electron_nano + 1;
         e_pt_nano[j]->Fill(nano_t->Electron_pt[nano_indices_e[j]]);
         e_eta_nano[j]->Fill(nano_t->Electron_eta[nano_indices_e[j]]);
         e_phi_nano[j]->Fill(nano_t->Electron_phi[nano_indices_e[j]]);
@@ -224,52 +231,64 @@ void LeptonsComp(){
   TCanvas* can = new TCanvas("can","can",500,500);
   can->SetLogy();
 
+
   for( int j= 0 ; j < 4 ; j++ ){
     sprintf(temp,"filteredElectron_pt%i.png",j);
     e_pt_ra2b[j]->Draw();
     e_pt_nano[j]->Draw("SAME");
+      can->BuildLegend();
     can->SaveAs(temp);
 
     sprintf(temp,"filteredElectron_eta%i.png",j);
     e_eta_ra2b[j]->Draw();
     e_eta_nano[j]->Draw("SAME");
+      can->BuildLegend();
     can->SaveAs(temp);
       
     sprintf(temp,"filteredElectron_phi%i.png",j);
     e_phi_ra2b[j]->Draw();
     e_phi_nano[j]->Draw("SAME");
+      can->BuildLegend();
     can->SaveAs(temp);
 
       
       sprintf(temp,"filteredMuon_pt%i.png",j);
       mu_pt_ra2b[j]->Draw();
       mu_pt_nano[j]->Draw("SAME");
+      can->BuildLegend();
       can->SaveAs(temp);
 
       sprintf(temp,"filteredMuon_eta%i.png",j);
       mu_eta_ra2b[j]->Draw();
       mu_eta_nano[j]->Draw("SAME");
+      can->BuildLegend();
       can->SaveAs(temp);
         
       sprintf(temp,"filteredMuon_phi%i.png",j);
       mu_phi_ra2b[j]->Draw();
       mu_phi_nano[j]->Draw("SAME");
+      can->BuildLegend();
       can->SaveAs(temp);
       
       sprintf(temp,"filteredPhoton_pt%i.png",j);
       pho_pt_ra2b[j]->Draw();
       pho_pt_nano[j]->Draw("SAME");
+      can->BuildLegend();
       can->SaveAs(temp);
 
       sprintf(temp,"filteredPhoton_eta%i.png",j);
       pho_eta_ra2b[j]->Draw();
       pho_eta_nano[j]->Draw("SAME");
+      can->BuildLegend();
       can->SaveAs(temp);
         
       sprintf(temp,"filteredPhoton_phi%i.png",j);
       pho_phi_ra2b[j]->Draw();
       pho_phi_nano[j]->Draw("SAME");
+      can->BuildLegend();
       can->SaveAs(temp);
   }
-  
+    cout << "Nano: " << num_electron_nano << endl;
+    cout << "RA2b: " << num_electron_ra << endl;
+ 
 }
