a0bfc16fadad9c297a334265c3e643a46549ddae
gabihamilton <gabihamilton@gmail.com>
2023-08-01 09:56:47 -0500

    all electrons

--- electrons.c
+++ electrons.c
@@ -2,20 +2,6 @@
 #include "RA2bNtupleV20.C"
 
 bool debug = false;
-int n=1;
-
-vector<int> filterLeptons(UInt_t number, Float_t* pt, double pt_threshold/*, Float_t* eta, double eta_threshold*/){
-
-    //int n=4;
-    vector<int> indices;
-    for( int i = 0 ; i < number ; i++ ){
-        if(pt[i] > pt_threshold && pt[i] < 240/*&& fabs(eta[i]) < 2*/){
-            //cout << "i: " << i << " pt: " << pt[i] << endl;
-            indices.push_back(i);
-        }
-    }
-    return indices;
-}
 
 
 //------------------------------------//
@@ -91,97 +77,45 @@ void electrons(){
 
     char temp[100];
     
+    TH1F *pt_ra2b = new TH1F("TreeMaker Pt", ";Electron p_T [GeV];Events",80,0,800);
+    pt_ra2b->SetLineWidth(2);
+    pt_ra2b->SetLineStyle(1);
+    pt_ra2b->SetLineColor(2);
     
-  
-    vector<TH1F*> e_pt_ra2b;
-    vector<TH1F*> e_pt_nano;
-    for( int i = 0 ; i < n ; i++ ){
-        //sprintf(temp,"Electronpt_ra2b%i",i);
-        sprintf(temp,"RA2b");
-        e_pt_ra2b.push_back(new TH1F(temp,";Electron p_T [GeV];Events",80,0,800));
-        e_pt_ra2b.back()->SetLineWidth(2);
-        e_pt_ra2b.back()->SetLineStyle(1);
-        e_pt_ra2b.back()->SetLineColor(2);
-        //sprintf(temp,"Electronpt_nano%i",i);
-        sprintf(temp,"Nano");
-        e_pt_nano.push_back(new TH1F(temp,";Electron p_T [GeV];Events",80,0,800));
-        e_pt_nano.back()->SetLineWidth(2);
-        e_pt_nano.back()->SetLineStyle(2);
-        e_pt_nano.back()->SetLineColor(4);
-    }
+    TH1F *pt_nano = new TH1F("Nano Pt", ";Electron p_T [GeV];Events",80,0,800);
+    pt_nano->SetLineWidth(2);
+    pt_nano->SetLineStyle(2);
+    pt_nano->SetLineColor(4);
 
-    vector<TH1F*> e_eta_ra2b;
-    vector<TH1F*> e_eta_nano;
-    for( int i = 0 ; i < n ; i++ ){
-        //sprintf(temp,"Electroneta_ra2b%i",i);
-        sprintf(temp,"RA2b");
-        e_eta_ra2b.push_back(new TH1F(temp,";Electron #eta [GeV];Events",50,-5,5));
-        e_eta_ra2b.back()->SetLineWidth(2);
-        e_eta_ra2b.back()->SetLineStyle(1);
-        e_eta_ra2b.back()->SetLineColor(2);
-        sprintf(temp,"Nano");
-        e_eta_nano.push_back(new TH1F(temp,";Electron #eta [GeV];Events",50,-5,5));
-        e_eta_nano.back()->SetLineWidth(2);
-        e_eta_nano.back()->SetLineStyle(2);
-        e_eta_nano.back()->SetLineColor(4);
-    }
+    TH1F* eta_ra2b = new TH1F("TreeMaker Eta",";Electron #eta [GeV];Events",50,-5,5);
+    eta_ra2b->SetLineWidth(2);
+    eta_ra2b->SetLineStyle(1);
+    eta_ra2b->SetLineColor(2);
     
-    vector<TH1F*> e_phi_ra2b;
-    vector<TH1F*> e_phi_nano;
-    for( int i = 0 ; i < n ; i++ ){
-        //sprintf(temp,"Electronphi_ra2b%i",i);
-        sprintf(temp,"RA2b");
-        e_phi_ra2b.push_back(new TH1F(temp,";Electron #phi [GeV];Events",50,-5,5));
-        e_phi_ra2b.back()->SetLineWidth(2);
-        e_phi_ra2b.back()->SetLineStyle(1);
-        e_phi_ra2b.back()->SetLineColor(2);
-        //sprintf(temp,"Electronphi_nano%i",i);
-        sprintf(temp,"Nano");
-        e_phi_nano.push_back(new TH1F(temp,";Electron #phi [GeV];Events",50,-5,5));
-        e_phi_nano.back()->SetLineWidth(2);
-        e_phi_nano.back()->SetLineStyle(2);
-        e_phi_nano.back()->SetLineColor(4);
-    }
-  
-    vector<TH1F*> e_iso_ra2b;
-    vector<TH1F*> e_iso_nano;
-    for( int i = 0 ; i < n ; i++ ){
-        //sprintf(temp,"Electron_iso_ra2b%i",i);
-        sprintf(temp,"RA2b");
-        e_iso_ra2b.push_back(new TH1F(temp,";Electron iso [GeV];Events",50,0,10));
-        e_iso_ra2b.back()->SetLineWidth(2);
-        e_iso_ra2b.back()->SetLineStyle(1);
-        e_iso_ra2b.back()->SetLineColor(2);
-        //sprintf(temp,"Electron_iso_nano%i",i);
-        sprintf(temp,"Nano");
-        e_iso_nano.push_back(new TH1F(temp,";Electron iso [GeV];Events",50,0,10));
-        e_iso_nano.back()->SetLineWidth(2);
-        e_iso_nano.back()->SetLineStyle(2);
-        e_iso_nano.back()->SetLineColor(4);
-    }
+    TH1F* eta_nano = new TH1F("Nano Eta",";Electron #eta [GeV];Events",50,-5,5);
+    eta_nano->SetLineWidth(2);
+    eta_nano->SetLineStyle(2);
+    eta_nano->SetLineColor(4);
+
+    TH1F* phi_ra2b = new TH1F("TreeMaker Phi",";Electron #phi [GeV];Events",50,-5,5);
+    phi_ra2b->SetLineWidth(2);
+    phi_ra2b->SetLineStyle(1);
+    phi_ra2b->SetLineColor(2);
     
-    vector<TH1F*> sieie_ra2b;
-    vector<TH1F*> sieie_nano;
-    for( int i = 0 ; i < n ; i++ ){
-        sprintf(temp,"Sieie_ra2b%i",i);
-        sieie_ra2b.push_back(new TH1F(temp,";Electron #phi [GeV];Events",50,0,1));
-        sieie_ra2b.back()->SetLineWidth(2);
-        sieie_ra2b.back()->SetLineStyle(1);
-        sieie_ra2b.back()->SetLineColor(2);
-        sprintf(temp,"Sieie_nano%i",i);
-        sieie_nano.push_back(new TH1F(temp,";Electron #phi [GeV];Events",50,0,0.015));
-        sieie_nano.back()->SetLineWidth(2);
-        sieie_nano.back()->SetLineStyle(2);
-        sieie_nano.back()->SetLineColor(4);
-    }
-    TH1F* e_all_nano = new TH1F(temp, "; All Electrons pT [GeV];Events", 50, 0, 800);
-    e_all_nano->SetLineWidth(2);
-    e_all_nano->SetLineStyle(2);
-    e_all_nano->SetLineColor(4);
-    TH1F* e_all_ra2b = new TH1F(temp, "; All Electrons pT [GeV];Events", 50, 0, 800);
-    e_all_ra2b->SetLineWidth(2);
-    e_all_ra2b->SetLineStyle(1);
-    e_all_ra2b->SetLineColor(2);
+    TH1F* phi_nano = new TH1F("Nano Phi",";Electron #phi [GeV];Events",50,-5,5);
+    phi_nano->SetLineWidth(2);
+    phi_nano->SetLineStyle(2);
+    phi_nano->SetLineColor(4);
+
+    TH1F* iso_ra2b = new TH1F("TreeMaker iso",";Electron iso",50,0,35);
+    iso_ra2b->SetLineWidth(2);
+    iso_ra2b->SetLineStyle(1);
+    iso_ra2b->SetLineColor(2);
+    
+    TH1F* iso_nano = new TH1F("Nano iso",";Electron iso",50,0,35);
+    iso_nano->SetLineWidth(2);
+    iso_nano->SetLineStyle(2);
+    iso_nano->SetLineColor(4);
 
     int e_pt, e_eta, e_phi;
     int numEvents = ra2b_t->fChain->GetEntriesFast();
@@ -191,139 +125,130 @@ void electrons(){
         ra2b_t->GetEntry(evt);
         nano_t->GetEntry(numEvents-evt-1);
 
-        
-        //vector<int> nano_indices_e = BitmapFilter(nano_t->nElectron, nano_t->Electron_vidNestedWPBitmap, nano_t->Electron_pt, 10.0);
-        
-        vector<int> ra2b_indices_e = filterLeptons(ra2b_t->Electrons_, ra2b_t->Electrons_fCoordinates_fPt, 200.0);
-        vector<int> nano_indices_e = filterLeptons(nano_t->nElectron, nano_t->Electron_pt, 200.0);
+        for( int j= 0 ; j < ra2b_t->Electrons_ ; j++ ){
 
-
-
-        for( int j= 0 ; j < n ; j++ ){
-            if( j < ra2b_indices_e.size() /*&& fabs(ra2b_t->Electrons_fCoordinates_fEta[ra2b_indices_e[j]])<1&& j<ra2b_t->Electrons_passIso->size() && j<ra2b_t->Electrons_mediumID->size()*/){
-                //bool iso = ra2b_t->Electrons_passIso->at(j);
-                //bool medium = ra2b_t->Electrons_mediumID->at(j);
-                //if(/*iso && medium&& fabs(ra2b_t->Electrons_fCoordinates_fEta[ra2b_indices_e[j]])<1*/ra2b_t->Electrons_mediumID->at(j)){
-                    //e_pt_ra2b[j]->Fill(ra2b_t->Electrons_fCoordinates_fPt[ra2b_indices_e[j]]);
-                    //e_eta_ra2b[j]->Fill(ra2b_t->Electrons_fCoordinates_fEta[ra2b_indices_e[j]]);
-                    //e_phi_ra2b[j]->Fill(ra2b_t->Electrons_fCoordinates_fPhi[ra2b_indices_e[j]]);
-                    //e_iso_ra2b[j]->Fill(ra2b_t->Electrons_iso->at(ra2b_indices_e[j]));
-                    //e_all_ra2b->Fill(ra2b_t->Electrons_fCoordinates_fPt[ra2b_indices_e[j]]);
-                    cout << evt << " RA  " << ra2b_indices_e[j] << endl;
-                //}
+            if(ra2b_t->Electrons_fCoordinates_fPt[j]>10.0 /*&& ra2b_t->Electrons_mediumID->at(j) && ra2b_t->Electrons_passIso->at(j)*/){
+                pt_ra2b->Fill(ra2b_t->Electrons_fCoordinates_fPt[j]);
+                eta_ra2b->Fill(ra2b_t->Electrons_fCoordinates_fEta[j]);
+                phi_ra2b->Fill(ra2b_t->Electrons_fCoordinates_fPhi[j]);
+                iso_ra2b->Fill(ra2b_t->Electrons_iso->at(j));
+                //cout << evt << " RA  " << ra2b_indices_e[j] << endl;
             }
-            if( j < nano_indices_e.size() ){
-                int id_nr = 1; //cut based
-                int nr_cuts = 10; //cut based has 9 cuts
-                int cut_size = 3;// bit size per cut is 3 bits
-                int cut_bits = make_cut_bits(nano_t->Electron_vidNestedWPBitmap[j],nr_cuts,cut_size,id_nr);
-                //if((cut_bits & 0x3EF) == 0x3EF){
-                //BITMAP
-                if (/*nano_t->Electron_cutBased[j]>0){ && fabs(nano_t->Electron_eta[nano_indices_e[j]])<1 && */nano_t->Electron_dxy[nano_indices_e[j]]<0.1 && nano_t->Electron_dz[nano_indices_e[j]]<0.2 && (cut_bits & 0x3FF) == 0x3FF){//except isolation: (cut_bits & 0x37F) == 0x37F){ //0:fail, 1:veto, 2:loose, 3:medium, 4:tight
-                    //e_pt_nano[j]->Fill(nano_t->Electron_pt[nano_indices_e[j]]);
-                    //e_eta_nano[j]->Fill(nano_t->Electron_eta[nano_indices_e[j]]);
-                    //e_phi_nano[j]->Fill(nano_t->Electron_phi[nano_indices_e[j]]);
-                    //e_iso_nano[j]->Fill(nano_t->Electron_miniPFRelIso_all[nano_indices_e[j]]);
-                    //e_all_nano->Fill(nano_t->Electron_pt[nano_indices_e[j]]);
-                    //sieie_nano[j]->Fill(nano_t->Electron_sieie[nano_indices_e[j]]);
-                    
-                    cout << evt << " NANO  " << nano_indices_e[j] << endl;
-                }
-                /*if(nano_t->Electron_cutBased[nano_indices_e[j]]>0){
-                    e_pt_ra2b[j]->Fill(nano_t->Electron_pt[nano_indices_e[j]]);
-                    e_eta_ra2b[j]->Fill(nano_t->Electron_eta[nano_indices_e[j]]);
-                    e_phi_ra2b[j]->Fill(nano_t->Electron_phi[nano_indices_e[j]]);
-                }*/
+            /*if(ra2b_t->Electrons_fCoordinates_fPt[j]>200 && ra2b_t->Electrons_fCoordinates_fPt[j]<220){
+                cout << "RA2b  " << ra2b_t->EvtNum << endl;
+                cout << "Pt  " << ra2b_t->Electrons_fCoordinates_fPt[j] << endl;
+                cout << "Eta  " << ra2b_t->Electrons_fCoordinates_fEta[j] << endl;
+                cout << "Phi  " << ra2b_t->Electrons_fCoordinates_fPhi[j] << endl;
+                cout << "Charge  " << ra2b_t->Electrons_charge->at(j) << endl;
+                cout << "Iso  " << ra2b_t->Electrons_iso->at(j) << endl;
+                cout << "MediumID  " << ra2b_t->Electrons_mediumID->at(j) << endl;
+                cout << "PassIso  " << ra2b_t->Electrons_passIso->at(j) << endl;
+                cout << "tightID  " << ra2b_t->Electrons_tightID->at(j) << endl;
+            }*/
+        }
+        
+        for(int j=0; j < nano_t->nElectron; j++ ){
+            int id_nr = 1; //cut based
+            int nr_cuts = 10; //cut based has 9 cuts
+            int cut_size = 3;// bit size per cut is 3 bits
+            int cut_bits = make_cut_bits(nano_t->Electron_vidNestedWPBitmap[j],nr_cuts,cut_size,id_nr);
+            //if((cut_bits & 0x3EF) == 0x3EF){
+            //BITMAP
+            if (nano_t->Electron_pt[j]>10.0 && nano_t->Electron_cutBased[j]>0 && nano_t->Electron_dxy[j]<0.1 && nano_t->Electron_dz[j]<0.2 && (cut_bits & 0x37F) == 0x37F){//except isolation: (cut_bits & 0x37F) == 0x37F){ //0:fail, 1:veto, 2:loose, 3:medium, 4:tight
+                pt_nano->Fill(nano_t->Electron_pt[j]);
+                eta_nano->Fill(nano_t->Electron_eta[j]);
+                phi_nano->Fill(nano_t->Electron_phi[j]);
+                //iso_nano->Fill(nano_t->Electron_miniPFRelIso_all[j]*nano_t->Electron_pt[j]);
+                iso_nano->Fill(nano_t->Electron_pfRelIso03_all[j]*nano_t->Electron_pt[j]);
+                //cout << evt << " NANO  " << nano_indices_e[j] << endl;
             }
+            
+            //if(nano_t->Electron_pt[j]>200 && nano_t->Electron_pt[j]<220){
+             /*if(nano_t->event==3112473 ||nano_t->event==31117312 || nano_t->event==32675621 || nano_t->event==32677578 || nano_t->event==719576 || nano_t->event==2764104 || nano_t->event==2768780 || nano_t->event==2104){
+                cout << "Nano   " << nano_t->event << endl;
+                cout << "Pt " << nano_t->Electron_pt[j] << endl;
+                cout << "Eta    " << nano_t->Electron_eta[j] << endl;
+                cout << "Phi    " << nano_t->Electron_phi[j] << endl;
+                cout << "Charge " << nano_t->Electron_charge[j] << endl;
+                cout << "Iso    " << nano_t->Electron_miniPFRelIso_all[j]*nano_t->Electron_pt[j] << endl;
+                cout << "CutBased   " << nano_t->Electron_cutBased[j] << endl;
+                cout << "PassIso    "  << endl;
+                 cout << "  " <<endl;
+            }*/
 
         }
     }
 
+
     TCanvas* can = new TCanvas("can","can",500,500);
     can->SetLogy();
 
+/*
+    pt_nano->Draw();
+    pt_ra2b->Draw("SAME");
+    can->BuildLegend(0.4,0.9, 0.6, 0.8);
+    can->SaveAs("E_pt_final.pdf");
+    //cout << e_pt_nano[j]->GetEntries() << endl;
+    // Create and draw the ratio plot
+    auto rp = new TRatioPlot(pt_ra2b,pt_nano);
+    can->SetTicks(0, 1);
+    rp->Draw();
+    rp->GetLowYaxis()->SetNdivisions(505);
+    rp->GetLowerRefYaxis()->SetTitle("diff");
+    rp->GetUpperPad()->cd();
+    TLegend *legend = new TLegend(0.3,0.7,0.7,0.85);
+    legend->AddEntry("TreeMaker Pt","TreeMaker Pt","l");
+    legend->AddEntry("Nano Pt","Nano Pt","l");
+    legend->Draw();
 
-    for( int j= 0 ; j < n ; j++ ){
-        //sprintf(temp,"Electron_pt%i.png",j);
-  /*      e_pt_nano[j]->Draw();
-        e_pt_ra2b[j]->Draw("SAME");
-        can->BuildLegend(0.4,0.9, 0.6, 0.8);
-        can->SaveAs("E_pt_final.pdf");
-        //cout << e_pt_nano[j]->GetEntries() << endl;
-        // Create and draw the ratio plot
-        auto rp = new TRatioPlot(e_pt_ra2b[j],e_pt_nano[j], "diff");
-        can->SetTicks(0, 1);
-        rp->Draw();
-        rp->GetLowYaxis()->SetNdivisions(505);
-        rp->GetLowerRefYaxis()->SetTitle("diff");
-        rp->GetUpperPad()->cd();
-        TLegend *legend = new TLegend(0.3,0.7,0.7,0.85);
-        legend->AddEntry("RA2b","RA2b","l");
-        legend->AddEntry("Nano","Nano","l");
-        legend->Draw();
-        cout << e_pt_nano[j]->GetEntries() << endl;
+    eta_nano->Draw();
+    eta_ra2b->Draw("SAME");
+    can->BuildLegend(0.4,0.9, 0.6, 0.8);
+    cout << eta_nano->GetEntries() << endl;
+    // Create and draw the ratio plot
+    auto rp = new TRatioPlot(eta_ra2b,eta_nano);
+    can->SetTicks(0, 1);
+    rp->Draw();
+    rp->GetLowYaxis()->SetNdivisions(505);
+    rp->GetUpperPad()->cd();
+    TLegend *legend = new TLegend(0.3,0.7,0.7,0.85);
+    legend->AddEntry("Nano Eta","Nano Eta","l");
+    legend->AddEntry("TreeMaker Eta","TreeMaker Eta","l");
+    legend->Draw();
 
-        sprintf(temp,"Electron_eta%i.png",j);
-        e_eta_nano[j]->Draw();
-        e_eta_ra2b[j]->Draw("SAME");
-        can->BuildLegend(0.4,0.9, 0.6, 0.8);
-        can->SaveAs(temp);
-        cout << e_eta_nano[j]->GetEntries() << endl;
-        // Create and draw the ratio plot
-        auto rp = new TRatioPlot(e_eta_ra2b[j],e_eta_nano[j],  "diff");
-        can->SetTicks(0, 1);
-        rp->Draw();
-        rp->GetLowYaxis()->SetNdivisions(505);
-        rp->GetUpperPad()->cd();
-        TLegend *legend = new TLegend(0.3,0.7,0.7,0.85);
-        legend->AddEntry("Nano","Nano","l");
-        legend->AddEntry("RA2b","RA2b","l");
-        legend->Draw();
-
-        sprintf(temp,"Electron_phi%i.png",j);
-        e_phi_nano[j]->Draw();
-        e_phi_ra2b[j]->Draw("SAME");
-        can->BuildLegend(0.4,0.9, 0.6, 0.8);
-        can->SaveAs(temp);
-        cout << e_phi_nano[j]->GetEntries() << endl;
-        // Create and draw the ratio plot
-        auto rp = new TRatioPlot(e_phi_ra2b[j],e_phi_nano[j],  "diff");
-        can->SetTicks(0, 1);
-        rp->Draw();
-        rp->GetLowYaxis()->SetNdivisions(505);
-        rp->GetUpperPad()->cd();
-        TLegend *legend = new TLegend(0.3,0.7,0.7,0.85);
-        legend->AddEntry("Nano","Nano","l");
-        legend->AddEntry("RA2b","RA2b","l");
-        legend->Draw();
-*/
-        sprintf(temp,"Electron_iso%i.png",j);
-        e_iso_nano[j]->Draw();
-        e_iso_ra2b[j]->Draw("SAME");
-        can->BuildLegend(0.4,0.9, 0.6, 0.8);
-        can->SaveAs(temp);
-        // Create and draw the ratio plot
-        auto rp = new TRatioPlot(e_iso_ra2b[j],e_iso_nano[j],  "diff");
-        can->SetTicks(0, 1);
-        rp->Draw();
-        rp->GetLowYaxis()->SetNdivisions(505);
-        rp->GetUpperPad()->cd();
-        TLegend *legend = new TLegend(0.3,0.7,0.7,0.85);
-        legend->AddEntry("Nano","Nano","l");
-        legend->AddEntry("RA2b","RA2b","l");
-        legend->Draw();
-        
-        /*sprintf(temp,"Sieie%i.png",j);
-        //e_iso_ra2b[j]->Draw();
-        sieie_nano[j]->Draw();
-        can->BuildLegend(0.4,0.9, 0.6, 0.8);
-        can->SaveAs(temp);*/
-    }
-    //e_all_ra2b->Draw();
-    //e_all_nano->Draw("SAME");
-    //can->SaveAs("Electron_pt_all.png");
+    phi_nano->Draw();
+    phi_ra2b->Draw("SAME");
+    can->BuildLegend(0.4,0.9, 0.6, 0.8);
+    cout << phi_nano->GetEntries() << endl;
+    // Create and draw the ratio plot
+    auto rp = new TRatioPlot(phi_ra2b,phi_nano);
+    can->SetTicks(0, 1);
+    rp->Draw();
+    rp->GetLowYaxis()->SetNdivisions(505);
+    rp->GetUpperPad()->cd();
+    //rp->GetLowerRefYaxis()->SetTitle("diff");
+    TLegend *legend = new TLegend(0.3,0.7,0.7,0.85);
+    legend->AddEntry("Nano Phi","Nano Phi","l");
+    legend->AddEntry("TreeMaker Phi","TreeMaker Phi","l");
+    legend->Draw();
+ 
+ */
+    iso_ra2b->Draw();
+    iso_nano->Draw("SAME");
+    can->BuildLegend(0.4,0.9, 0.6, 0.8);
+    cout << phi_nano->GetEntries() << endl;
+    // Create and draw the ratio plot
+    auto rp = new TRatioPlot(iso_nano,iso_ra2b);
+    rp->Draw();
+    rp->GetLowYaxis()->SetNdivisions(505);
+    rp->GetUpperPad()->cd();
+    //rp->GetLowerRefYaxis()->SetTitle("diff");
+    TLegend *legend = new TLegend(0.3,0.7,0.7,0.85);
+    legend->AddEntry("Nano iso","Nano iso","l");
+    legend->AddEntry("TreeMaker iso","TreeMaker iso","l");
+    legend->Draw();
     
-        
+   
 }
     
 
